821c3abdcd4391519d90134cd4ea290b6ce3a177
Johannes Gräger <jgraeger@posteo.de>
2021-07-05 13:36:27 +0200

    Parsedown Extension

---
+++ Parser/Parsedown.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\OfflinePayments\Parser;
+
+
+class MarkdownParser extends ParsedownExtra
+{
+    protected function inlineEmphasis($Excerpt)
+    {
+        $res = parent::inlineEmphasis($Excerpt);
+        
+        
+        $res['element']['attributes'] = array(
+            'style' => $this->bootstrapTextClass($res['element']['name']),
+        );
+
+        return $res;
+    }
+
+    private function bootstrapTextClass($emphasis) 
+    {
+        if ($emphasis === 'strong')
+            return 'font-weight-bold';
+
+        if ($emphasis === 'em')
+            return 'font-weight-light';
+
+        return '';
+    }
+}
